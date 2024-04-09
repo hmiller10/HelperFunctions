5176abe22c17a770e3a85da8ddb565e79b66a3d2
HMiller <heathermiller688@gmail.com>
2024-04-08 23:14:09 -0500

    Pester test updates

--- Tests/Get-ComputerNameByIP.Tests.ps1
+++ Tests/Get-ComputerNameByIP.Tests.ps1
@@ -1,28 +1,33 @@
-﻿BeforeAll {
-	Import-Module -Name HelperFunctions -Force
-	Import-Module -Name Pester -Force
-	
+﻿BeforeAll {	
 	if ($Error)
 	{
 		$Error.Clear()
 	}
-	$IPAddress = (Get-CimInstance -ClassName Win32_NetworkAdapterConfiguration -Namespace 'root\CIMv2' -Filter "IPEnabled = 'True'" | Where { $_.DefaultIPGateway -ne $null } | Select-Object -Property IPAddress).IPAddress
-	[IPAddress]$IPAddress = $IPAddress[0]
+	
 }
 
 Describe 'Get-ComputerNameByIP' {
 
 	Context "Get-ComputerNameByIP is called, it must have parameter IPAddress" {
 		# Get-ComputerNameByIP Tests, all should pass
-
+		BeforeEach {
+			$cmd = Get-Command -Name Get-ComputerNameByIP -Module HelperFunctions -CommandType Function
+			$IPAddress = (Get-CimInstance -ClassName Win32_NetworkAdapterConfiguration -Namespace 'root\CIMv2' -Filter "IPEnabled = 'True'" | Where { $_.DefaultIPGateway -ne $null } | Select-Object -Property IPAddress).IPAddress
+			[IPAddress]$IPAddress = $IPAddress[0]
+		}
 		It "Get-ComputerNameByIP should have IPAddress as a mandatory parameter." {
-			Get-Command Get-ComputerNameByIP | Should -HaveParameter IPAddress -Because "IPAddress is required to render result."
+			$cmd | Should -HaveParameter IPAddress -Because "IPAddress is required to render result."
 		}
 
 		It "Should return a string value" {
 			$result = Get-ComputerNameByIP -IPAddress $IPAddress
 			$result | Should -Not -BeNullOrEmpty
 			$result | Should -BeOfType [System.String]
+		}
+
+		AfterEach {
+			$null = $IPAddress
+			$null = $cmd
 		}
 	}
 }
--- Tests/Get-DNfromFQDN.Tests.ps1
+++ Tests/Get-DNfromFQDN.Tests.ps1
@@ -1,20 +1,21 @@
 ﻿BeforeAll {
-	Import-Module -Name HelperFunctions -Force
-	Import-Module -Name Pester -Force
-	if ($Error) { $Error.Clear() }
 
+	if ($Error) { $Error.Clear() }
 	#$ComputerFQDN = [System.Net.Dns]::GetHostByName("LocalHost").HostName
-	$ComputerFQDN = "computer1.my.domain.com"
+	
 }
 
 Describe 'Get-DNfromFQDN' {
 
 	Context "Return Get-DNfromFQDN" {
 		# Get-DNfromFQDN Tests, all should pass
-
+		BeforeEach {
+			$ComputerFQDN = "computer1.my.domain.com"
+		}
+		
 		It "Get-DNfromFQDN should have parameter DomainFQDN." {
-			Get-Command Get-DNfromFQDN | Should -HaveParameter FQDN -Type String -Mandatory -Because "Function must have object FQDN to process."
 			Get-Command Get-DNfromFQDN | Should -Not -BeNullOrEmpty
+			Get-Command Get-DNfromFQDN | Should -HaveParameter FQDN -Type String -Mandatory -Because "Function must have object FQDN to process."
 		}
 
 		It "Should be of type [System.String]" {
