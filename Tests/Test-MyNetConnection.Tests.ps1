2cc3863db88b7afb7430c932d12a62bd6524a3ef
HMiller <heathermiller688@gmail.com>
2024-04-20 09:38:56 -0500

    Another mtnc test revision

--- Tests/Test-MyNetConnection.Tests.ps1
+++ Tests/Test-MyNetConnection.Tests.ps1
@@ -15,11 +15,16 @@
 
 Describe "Test-MyNetConnection" {
 	
+	Context "Test parameters" {
+		It "Should have verified parameters" {
+			Get-Command Test-MyNetConnection -ServerName $ServerIP -Port $Port -Module HelperFunctions -CommandType Function | Should -HaveParameter -ParameterName ServerName -Type System.String -Because "The function requires a destination to test."
+			Get-Command Test-MyNetConnection -ServerName $ServerIP -Port $Port -Module HelperFunctions -CommandType Function | Should -HaveParameter -ParameterName Port -Type int32 -Because "This function is intended to test port connectivity."
+		}
+	}
+
 	Context "Testing network connectivity and parameters" {
 		It "Should resolve DNS name 'google.com'" {
-			$result = Test-MyNetConnection -Server $google -Port $SecurePort
-			$result | Should -HaveParameter Server
-			$result | Should -HaveParameter Port
+			$result = Test-MyNetConnection -ServerName $google -Port $SecurePort
 			$result | Should -Not -BeNullOrEmpty
 			$result.TcpTestSucceeded | Should -Be $true
 		}
@@ -27,13 +32,13 @@ Describe "Test-MyNetConnection" {
 	
 	Context "Testing specific ports" {
 		It "Should check if port 80 (HTTP) is open on yahoo.com" {
-			$result = Test-MyNetConnection -Server $remoteDomain1 -Port $Port
+			$result = Test-MyNetConnection -ServerName $remoteDomain1 -Port $Port
 			$result | Should -Not -BeNullOrEmpty
 			$result.TcpTestSucceeded | Should -Be $true
 		}
 		
 		It "Should check if port 443 (HTTPS) is open on microsoft.com" {
-			$result = Test-MyNetConnection -Server $remoteDomain2 -Port $SecurePort
+			$result = Test-MyNetConnection -ServerName $remoteDomain2 -Port $SecurePort
 			$result | Should -Not -BeNullOrEmpty
 			$result.TcpTestSucceeded | Should -Be $true
 		}
