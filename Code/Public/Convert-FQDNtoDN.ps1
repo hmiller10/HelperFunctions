57da382559219272fbcebebb598abcb3b4cdfa65
Heather Miller <heathermiller688@gmail.com>
2024-05-06 16:53:28 -0500

    Adding new function and readme files, cleaning bad script data

---
+++ Code/Public/Convert-FQDNtoDN.ps1
@@ -0,0 +1,38 @@
+﻿function global:Convert-FQDNtoDN
+{
+	<#
+		.EXTERNALHELP HelperFunctions.psm1-Help.xml
+	#>
+	
+	[CmdletBinding()]
+	[OutputType([string])]
+	param
+	(
+		[Parameter(Mandatory = $true,
+				 HelpMessage = 'Enter domain FQDN')]
+		[ValidateNotNullOrEmpty()]
+		[string]$FQDN
+	)
+	
+	begin
+	{
+		$DomainFQDNArray = $FQDN -Split ("\.")
+	}
+	process
+	{
+		[int]$FQDNCount = 0
+		
+		foreach ($Item in $DomainFQDNArray)
+		{
+			if ($FQDNCount -eq 0)
+			{ [string]$objDNArrayItem += "DC=" + $Item }
+			else
+			{ [string]$objDNArrayItem += ",DC=" + $Item }
+			$FQDNCount++
+		}
+	}
+	end
+	{
+		return $objDNArrayItem
+	}
+}#end function Convert-FQDNtoDN
---
+++ Tests/Convert-FQDNToDN.Tests.ps1
@@ -0,0 +1,26 @@
+﻿BeforeAll {
+	Import-Module -Name HelperFunctions -Force
+	Import-Module -Name Pester -Force
+	if ($Error) { $Error.Clear() }
+	$FQDN = "my.domain.com"
+}
+
+Describe 'Convert-FQDNToDN' {
+	Context "Test Function Parameters" {
+		# Convert-FQDNToDN Tests, all should pass
+
+		It "Convert-FQDNToDN should have parameter FQDN." {
+		  Get-Command Convert-FQDNToDN | Should -HaveParameter FQDN -Type String -Mandatory
+		}
+
+		It "Should be of type [System.String]" {
+			$result = Convert-FQDNToDN -FQDN $FQDN
+			$result | Should -Not -BeNullOrEmpty
+			$result | Should -ExpectedType [System.String]
+		}
+	}
+}
+
+AfterAll {
+	$Error.Clear()
+}
