fab34e0d60f88d0366f66608975d0dcf09ef6460
Heather Miller <heathermiller688@gmail.com>
2024-06-27 17:30:33 -0500

    Updated Pester tests again

--- Tests/Test-PathExists.Tests.ps1
+++ Tests/Test-PathExists.Tests.ps1
@@ -1,32 +1,30 @@
 ﻿BeforeAll {
-	
+	Import-Module -Name HelperFunctions -Force -ErrorAction Stop
 	Import-Module -Name Pester -Force
-    if ($Error) { $Error.Clear() }
+	if ($Error) { $Error.Clear() }
 }
 
 Describe 'Test-PathExists' {
 
-  ## This ensures New-Item will never run. It's just being used as a 
-    ## flag to test if it attempts to execute
-    mock 'Test-PathExists'
+	## This ensures New-Item will never run. It's just being used as a
+	## flag to test if it attempts to execute
+	mock 'Test-PathExists'
 
-    context 'when the file path does not exist' {
+	context 'when the file path does not exist' {
 
-        Import-Module -Name HelperFunctions -Force
+		## This ensures Test-Path always returns $false "mimicking" the file does not exist
+		mock 'Test-Path' -ModuleName HelperFunctions  { $false }
 
-        ## This ensures Test-Path always returns $false "mimicking" the file does not exist
-        mock 'Test-Path' -ModuleName HelperFunctions  { $false }
-        
-        $null = Test-PathExists -Path '~\file.txt' -PathType File
+		$null = Test-PathExists -Path '~\file.txt' -PathType File
 
-        it 'creates the file' {
-            ## This checks to see if New-Item attempted to run. If so, we know the script did what we expected
-            $assMParams = @{
-                CommandName = 'Test-Path'
-                Times = 1
-                Exactly = $true
-            }
-            Assert-MockCalled -CommandName Test-PathExists -Times 1 -ModuleName HelperFUnctions -Scope Context
-        }
-    }
+		it 'creates the file' {
+			## This checks to see if New-Item attempted to run. If so, we know the script did what we expected
+			$assMParams = @{
+				CommandName = 'Test-Path'
+				Times	  = 1
+				Exactly     = $true
+			}
+			Assert-MockCalled -CommandName Test-PathExists -Times 1 -ModuleName HelperFUnctions -Scope Context
+		}
+	}
 }
