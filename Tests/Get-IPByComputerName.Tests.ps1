0f41d30ef581f7a8295ddfeaf904b62ad6b5967b
HMiller <heathermiller688@gmail.com>
2024-08-20 00:23:02 -0500

    Refactored pester tests

--- Tests/Get-IPByComputerName.Tests.ps1
+++ Tests/Get-IPByComputerName.Tests.ps1
@@ -2,23 +2,26 @@
 	if ($Error) { $Error.Clear() }
 	Import-Module -Name HelperFunctions -Force
 	$Computer = [System.Net.Dns]::GetHostByName("LocalHost").HostName
+	
 }
 
 Describe 'Get-IPByComputerName' {
+
 	Context "When Get-IPByComputerName is called" {
+
 		# Get-ComputerNameByIP Tests, all should pass
+
 		It "Get-IPByComputerName should have parameter ComputerName" {
-			Get-Command -Name Get-IPByComputerName -ComputerName $Computer -Module HelperFunctions -CommandType Function | Should -HaveParameter -ParameterName ComputerName
+			Get-Command Get-IPByComputerName | Should -HaveParameter ComputerName -Type [System.String[]] -Mandatory -Because "If IP address is valid it should return the assigned device."
 		}
-	}
-	Context "Test Get-IPByComputerName output" {
+
 		It "Get-IPByComputerName should return the IP address of the computer passed into the function" {
-			
-			$result = Get-IPByComputerName -ComputerName $Computer -ErrorAction Stop
-			$result | Should -BeOfType PSCustomObject -Because "A networked computer object should have an IP address."
+			$result = Get-IPByComputerName -ComputerName $Computer
 			$result | Should -Not -BeNullOrEmpty -Because "Cannot resolve an IP address if the computer name is invalid."
+			$result | Should -BeOfType [PSCustomObject]
 		}
 	}
+
 }
 
 AfterAll {
