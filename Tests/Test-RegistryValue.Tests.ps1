28db13d96fde538ba9d5beefe53203fedf63899d
HMiller <heathermiller688@gmail.com>
2024-10-26 14:36:19 -0500

    Refactored test after round 3 of testing

--- Tests/Test-RegistryValue.Tests.ps1
+++ Tests/Test-RegistryValue.Tests.ps1
@@ -5,8 +5,8 @@
 	{
 		$Error.Clear()
 
-		[string]$key = 'HKLM:\Software\Microsoft\Windows NT\CurrentVersion'
-		[string]$value = "CurrentBuild"
+		[string]$key = 'HKLM:\Software\HerThoughtsOnTech'
+		[string]$value = "BuildVersion"
 	}
 }
 
@@ -24,33 +24,22 @@ Describe "Test-RegistryValue Parameters" {
 			Get-Command Test-RegistryValue | Should -HaveParameter Name -Mandatory -Type System.String
 		}
 	}
-}
 
-# Pester Test
-Describe "Test-RegistryValue" {
-	It "Checks if a registry key exists" {
-		
-		# Ensure the key exists for the test
-		if (-Not (Test-Path "HKLM:\$key"))
-		{
-			New-Item -Path "HKLM:\$key" -Force
+	Context "Test for predefined registry key and value" {
+		It "Checks if a registry key/value pair exists" {
+			
+			# Ensure the key and value exist for the test
+			if (-Not (Test-Path "HKLM:\$key"))
+			{
+				New-Item -Path "HKLM:\$key" -Force
+			}
+			if (-Not (Get-ItemProperty -Path "HKLM:\$key" -Name $value -ErrorAction SilentlyContinue))
+			{
+				New-ItemProperty -Path "HKLM:\$key" -Name $value -Value "Test" -Force
+			}
+			
+			(Test-RegistryValue -Path $key -Name $value) | Should -BeTrue
 		}
-		
-		(Test-RegistryValue -Path $key -Name $value) | Should -BeTrue
 	}
 	
-	It "Checks if a registry key/value pair exists" {
-		
-		# Ensure the key and value exist for the test
-		if (-Not (Test-Path "HKLM:\$key"))
-		{
-			New-Item -Path "HKLM:\$key" -Force
-		}
-		if (-Not (Get-ItemProperty -Path "HKLM:\$key" -Name $value -ErrorAction SilentlyContinue))
-		{
-			New-ItemProperty -Path "HKLM:\$key" -Name $value -Value "Test" -Force
-		}
-		
-		(Test-RegistryValue -Path $key -Name $value) | Should -BeTrue
-	}
 }
