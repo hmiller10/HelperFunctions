6d4c4c2612233e48dfae820531b81c0e7e19a0b6
HMiller <heathermiller688@gmail.com>
2024-10-26 14:16:14 -0500

    Refactored Pester test Describe blocks

--- Tests/Test-RegistryValue.Tests.ps1
+++ Tests/Test-RegistryValue.Tests.ps1
@@ -11,7 +11,7 @@
 }
 
 
-Describe "Test-RegistryValue" {
+Describe "Test-RegistryValue Parameters" {
 
 	Context "Test pre-defined registry key and value" {
 		# Test-RegistryValue Tests, all should pass
@@ -23,11 +23,37 @@ Describe "Test-RegistryValue" {
 		It "Should Have Parameter Value" {
 			Get-Command Test-RegistryValue | Should -HaveParameter Name -Mandatory -Type System.String
 		}
+	}
+}
 
-		It "Should be of type [System.Boolean]" {
-			$result = Test-RegistryValue -Path $key -Name $value
-			$result | Should -Not -BeNullOrEmpty
-			$result | Should -ExpectedType [bool]
+# Pester Test
+Describe "Test-RegistryValue" {
+	It "Checks if a registry key exists" {
+		$keyPath = "Software\HerThoughtsOnTech\TestApp"
+		
+		# Ensure the key exists for the test
+		if (-Not (Test-Path "HKLM:\$keyPath"))
+		{
+			New-Item -Path "HKLM:\$keyPath" -Force
 		}
+		
+		(Test-RegistryValue -Path $keyPath -Name $null) | Should -BeTrue
+	}
+	
+	It "Checks if a registry key/value pair exists" {
+		$keyPath = "Software\HerThoughtsOnTech\TestApp"
+		$valueName = "TestValue"
+		
+		# Ensure the key and value exist for the test
+		if (-Not (Test-Path "HKLM:\$keyPath"))
+		{
+			New-Item -Path "HKLM:\$keyPath" -Force
+		}
+		if (-Not (Get-ItemProperty -Path "HKLM:\$keyPath" -Name $valueName -ErrorAction SilentlyContinue))
+		{
+			New-ItemProperty -Path "HKLM:\$keyPath" -Name $valueName -Value "Test" -Force
+		}
+		
+		(Test-RegistryValue -Path $keyPath -Name $valueName) | Should -BeTrue
 	}
 }
