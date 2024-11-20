76ecd4115dcd4f3e17cf08b1272fc6646a920a92
HMiller <heathermiller688@gmail.com>
2024-11-19 23:25:39 -0600

    Updated help and Pester Tests

--- Tests/Test-PathExists.Tests.ps1
+++ Tests/Test-PathExists.Tests.ps1
@@ -1,50 +1,50 @@
-﻿BeforeAll {
+BeforeAll {
 	Import-Module -Name HelperFunctions -Force
 	Import-Module -Name Pester -Force
 	if ($Error) { $Error.Clear() }
+	# Define the path to the file or folder to be checked 
+     $Path = TestDrive:\tmpPath
+     $File = TestDrive:\text.txt
 }
 
-Describe 'Test-PathExists' {
-	
-	context 'Test function parameters' {
-		It "Should Have Parameter Path" {
-			Get-Command Test-PathExists | Should -HaveParameter Path -Mandatory -Type System.String
-		}
-		It "Should Have Parameter PathType" {
-			Get-Command Test-PathExists | Should -HaveParameter PathType -Mandatory -Type System.String
-		}
-		It "Should Have Parameter Force" {
-			Get-Command Test-PathExists | Should -HaveParameter Force -Type System.Management.Automation.SwitchParameter
+Describe 'Test-PathExists - Parameters' {
+	Context "Test Function Parameters" {
+		# Test-PathExists Tests, all should pass
+		
+		It "Test-PathExists should have parameter Path." {
+			Get-Command Test-PathExists | Should -HaveParameter Path -Type String -Mandatory
 		}
 		
+		It "Test-PathExists should have parameter PathType." {
+			Get-Command Test-PathExists | Should -HaveParameter PathTpe -Type String -Mandatory
+		}
+		
+		It "Should be of type [System.String]" {
+			$result = Test-PathExists -Path $Path -PathType Folder
+			$result | Should -Not -BeNullOrEmpty
+			$result | Should -ExpectedType [System.String]
+		}
 	}
-	
-	context "Test-Or-Create" {
-		It "Creates a file if it does not exist" {
-			$testFilePath = "C:\temp\testfile.txt"
-			
-			if (Test-Path $testFilePath)
-			{
-				Remove-Item $testFilePath
-			}
-			
-			Test-PathExists -Path $testFilePath -PathType File
-			
-			(Test-Path -Path $testFilePath) | Should -BeTrue
+}
+
+# Pester test to check for the existence of the file or folder
+Describe 'Test-PathExists - Folder' {
+	 It 'should exist' {
+		if (-Not (Test-Path -Path $Path -PathType Container))
+		{
+			New-Item -Path $Path -ItemType Directory
+		} 
+		Test-Path -Path $Path | Should -Be $true 
+	}
+}
+
+Describe 'Test-PathExists - File' {
+	It 'should exist' {
+		if (-Not (Test-Path -Path $File -PathType Leaf))
+		{
+			New-Item -Path $Path -ItemType File
 		}
-		
-		It "Creates a folder if it does not exist" {
-			$testFolderPath = "C:\temp\testfolder"
-			
-			if (Test-Path $testFolderPath)
-			{
-				Remove-Item $testFolderPath -Recurse
-			}
-			
-			Test-PathExists -Path $testFolderPath -PathType Folder
-			
-			(Test-Path -Path $testFolderPath) | Should -BeTrue
-		}
+		Test-Path -Path $Path | Should -Be $true
 	}
 }
 
